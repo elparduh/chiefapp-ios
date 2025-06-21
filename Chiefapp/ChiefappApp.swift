3149a3123e0d016e157fab0f0ad43ca50bffc081
Juan Ticante Vicente <ticantevicentejuan@outlook.com>
2025-06-20 18:08:02 -0600

    Initial Commit

---
+++ Chiefapp/ChiefappApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChiefappApp.swift
+//  Chiefapp
+//
+//  Created by Canelito on 20/06/25.
+//
+
+import SwiftUI
+
+@main
+struct ChiefappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
